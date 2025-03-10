80aace880c17ae732ba4cc40bfdf8fac9eefb80a
prakit.w <prakit.w@arise.tech>
2025-03-10 23:03:25 +0700

    statement to insert data to db

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 from sentence_transformers import SentenceTransformer
+import os
+import psycopg2
+
+ENV = {
+    "POSTGRES_DB": os.environ.get("POSTGRES_DB"),
+    "POSTGRES_USER": os.environ.get("POSTGRES_USER"),
+    "POSTGRES_PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+    "POSTGRES_PORT": os.environ.get("POSTGRES_PORT"),
+}
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(f'dbname={ENV["POSTGRES_DB"]} user={ENV["POSTGRES_USER"]} password={ENV["POSTGRES_PASSWORD"]} host=localhost port={ENV["POSTGRES_PORT"]}')
+cur = conn.cursor()
+
+def insert_document(text, vector):
+    # Insert text and vector into the database
+    cur.execute("INSERT INTO documents (text, vector) VALUES (%s, %s)", (text, vector.tolist()))
+    conn.commit()
 
 # 1. Load a pretrained Sentence Transformer model
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("BAAI/bge-m3")
 
 # The sentences to encode
-sentences = [
-    "The weather is lovely today.",
-    "It's so sunny outside!",
-    "He drove to the stadium.",
-]
+sentences = "Hello world"
 
 # 2. Calculate embeddings by calling model.encode()
-embeddings = model.encode(sentences)
-print(embeddings.shape)
+# embeddings = model.encode(sentences)
+# print(embeddings.tolist())
+# insert_embedding(sentences, embeddings)
+# print(embeddings.shape)
 # [3, 384]
 
 # 3. Calculate the embedding similarities
-similarities = model.similarity(embeddings, embeddings)
-print(similarities)
+# similarities = model.similarity(embeddings, embeddings)
+# print(similarities)
 # tensor([[1.0000, 0.6660, 0.1046],
 #         [0.6660, 1.0000, 0.1411],
 #         [0.1046, 0.1411, 1.0000]])
