4ff7d81880e985e3c0fd681d93a38966461d3280
prakit.w <prakit.w@arise.tech>
2025-03-11 22:22:36 +0700

    add fn load model in main.py

--- main.py
+++ main.py
@@ -19,21 +19,29 @@ def insert_document(text, vector):
     conn.commit()
 
 # 1. Load a pretrained Sentence Transformer model
-model = SentenceTransformer("BAAI/bge-m3")
+def load_model():
+    if os.path.exists("./models/bge-m3"):
+        return SentenceTransformer("./models/bge-m3")
+    else:
+        model = SentenceTransformer("BAAI/bge-m3")
+        model.save("./models/bge-m3")
+        return model
+
+model = load_model()
 
 # The sentences to encode
 sentences = "Hello world"
 
 # 2. Calculate embeddings by calling model.encode()
-# embeddings = model.encode(sentences)
-# print(embeddings.tolist())
+embeddings = model.encode(sentences)
+embedding2 = model.encode("aaa")
+print(embeddings.tolist())
 # insert_embedding(sentences, embeddings)
-# print(embeddings.shape)
-# [3, 384]
+print(embeddings.shape)
 
 # 3. Calculate the embedding similarities
-# similarities = model.similarity(embeddings, embeddings)
-# print(similarities)
+similarities = model.similarity(embeddings, embedding2)
+print(similarities.tolist())
 # tensor([[1.0000, 0.6660, 0.1046],
 #         [0.6660, 1.0000, 0.1411],
 #         [0.1046, 0.1411, 1.0000]])
